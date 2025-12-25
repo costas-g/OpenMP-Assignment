ecb80ccb282f1fe0b1a681f843b9670410eae4fb
costas-g <210527419+costas-g@users.noreply.github.com>
2025-12-25 11:24:01 +0200

    Tuning TASK_CUTOFF

--- ex3/src/merge_parallel.c
+++ ex3/src/merge_parallel.c
@@ -39,7 +39,10 @@ void parallel_merge(int *arr, int *tmp, long long l, long long m, long long r) {
         arr[i] = tmp[i];
     }
 }
+
 #include <stdio.h>
+int DEBUG = 0;
+
 void parallel_mergesort(int *arr, int *tmp, long long l, long long r) {
     // printf("Thread %d > mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), l, r);
     if (l < r) {
@@ -48,17 +51,17 @@ void parallel_mergesort(int *arr, int *tmp, long long l, long long r) {
 
         int task_enable = curr_size > TASK_CUTOFF;
 
-        // printf("Thread %d > Task mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), l, mid);
+        if (task_enable && DEBUG) printf("Thread %d > Task mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), l, mid);
         # pragma omp task if (task_enable) shared(arr, tmp) firstprivate(l, mid)
         {
-            // if (task_enable) printf("Thread %d > mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), l, mid);
+            if (task_enable && DEBUG) printf("Thread %d > mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), l, mid);
             parallel_mergesort(arr, tmp, l, mid);
         }
-    
-        // printf("Thread %d > Task mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), mid + 1, r);
+
+        if (task_enable && DEBUG) printf("Thread %d > Task mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), mid + 1, r);
         # pragma omp task if (task_enable) shared(arr, tmp) firstprivate(mid, r)
         {
-            // if (task_enable) printf("Thread %d > mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), mid + 1, r);
+            if (task_enable && DEBUG) printf("Thread %d > mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), mid + 1, r);
             parallel_mergesort(arr, tmp, mid + 1, r);
         }
         
@@ -70,12 +73,20 @@ void parallel_mergesort(int *arr, int *tmp, long long l, long long r) {
 
 void start_parallel_mergesort(int *arr, int *tmp, long long l, long long r, size_t thread_count) {
     long long size = r - l + 1;
-    size_t avg_tasks_per_thread = 64; /* 2-8? */
+    size_t avg_tasks_per_thread = 4; /* 2-8? */
     TASK_CUTOFF = size/(avg_tasks_per_thread * thread_count);
+    /* or */
+    size_t max_level = 2; /* max depth level to spawn new tasks */
+    TASK_CUTOFF = size/(1u << max_level);
+    /* or */
+    // TASK_CUTOFF = 20000000;
 
     # pragma omp parallel num_threads(thread_count)
     {
         # pragma omp single 
-        parallel_mergesort(arr, tmp, l, r);
+        {
+            if (DEBUG) printf("Thread %d > mergesort(arr, %lld, %lld)\n", omp_get_thread_num(), l, r);
+            parallel_mergesort(arr, tmp, l, r);
+        }
     }
 }
