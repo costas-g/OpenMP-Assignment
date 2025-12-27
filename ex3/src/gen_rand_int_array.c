013e55b137f7aac12d38dc1a7c48e1993840bc92
costas-g <210527419+costas-g@users.noreply.github.com>
2025-12-27 07:19:55 +0200

    minor changes to ex1 and ex3

--- ex3/src/gen_rand_int_array.c
+++ ex3/src/gen_rand_int_array.c
@@ -3,7 +3,7 @@
 
 #include "gen_rand_int_array.h"
 
-/* Allocates an array of <size> integers and returns a pointer to it */
+/* Allocates an array of SIZE integers, fills it with random values, and returns a pointer to it */
 int *gen_rand_int_array(long long size){
     int *arr = malloc((size_t)(size) * sizeof(int));
     if (!arr) {
@@ -11,7 +11,7 @@ int *gen_rand_int_array(long long size){
         exit(EXIT_FAILURE);
     }
 
-    for (long long i = 0; i <= size; i++){
+    for (long long i = 0; i < size; i++){
         arr[i] = (rand() % RAND_MAX);// - RAND_MAX/2; /* in range [-RAND_MAX/2, RAND_MAX/2 - 1] */
     }
 
